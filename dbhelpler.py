b7f05b9a71d35623dd13f0addf126e33947d0107
Krish Rastogi <70422720+krishras23@users.noreply.github.com>
2021-06-14 21:56:52 -0700

    Read and write path for trello

--- dbhelpler.py
+++ dbhelpler.py
@@ -1,66 +1,39 @@
 from mysql.connector import connect, Error
 
 
-def get_databases():
+# read path
+def get_tasks():
     try:
         with connect(
                 host="localhost",
                 user='root',
                 password='tomato',
         ) as connection:
-            # create_db_query = "CREATE DATABASE adventure32"
-            # with connection.cursor() as cursor:
-            #     cursor.execute(create_db_query)
-
-            db_list = []
-            show_db_query = "SHOW DATABASES"
+            task_list = []
+            get_tasks_query = "select * from trello.tasks"
             with connection.cursor() as cursor:
-                cursor.execute(show_db_query)
-                for db in cursor:
-                    print(db[0])
-                    db_list.append(db)
-                return db_list
-    except Error as e:
-        print(e)
-
-
-def get_students():
-    try:
-        with connect(
-                host="localhost",
-                user='root',
-                password='tomato',
-        ) as connection:
-            db_list = []
-            get_student_query = "select * from test.student"
-            with connection.cursor() as cursor:
-                cursor.execute(get_student_query)
-                print("running query")
+                cursor.execute(get_tasks_query)
                 for record in cursor:
-                    print(record[0])
-                    print(record[1])
-                    db_list.append(record)
-                return db_list
+                    task_list.append(record)
+                return task_list
     except Error as e:
         print(e)
 
 
-def insert_student(student_name, student_age):
+# write path
+
+def create_task(the_task_name, the_task_importance, the_task_owner):
     try:
         with connect(
                 host="localhost",
                 user='root',
                 password='tomato',
         ) as connection:
-            create_student_query = "insert into test.student (name, age) values (%s, %s)"
-            val = (student_name, student_age)
+            create_task_query = "insert into trello.tasks (task_name, importance, owner) values (%s, %s, %s)"
+            val = (the_task_name, the_task_importance, the_task_owner)
             with connection.cursor() as cursor:
-                cursor.execute(create_student_query, val)
+                cursor.execute(create_task_query, val)
                 connection.commit()
                 return
     except Error as e:
         print(e)
-
-
-if __name__ == '__main__':
-    insert_student("kapil", 33)
